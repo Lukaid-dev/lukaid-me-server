99098fa5788cb7bd5a1e8db0a8f810409aef34be
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-05-01 01:02:58 +0900

    tag api postid에 순서 미적용 수정

--- apps/posts/serializers/post_id.py
+++ apps/posts/serializers/post_id.py
@@ -23,4 +23,10 @@ class PostIdGetSerializer(serializers.ModelSerializer):
         )
 
     def get_tag_list(self, obj):
-        return [{"id": tag.id, "name": tag.name} for tag in obj.tags.all()]
+        return [
+            {
+                "name": post_tag.tag.name,
+                "order": post_tag.order,
+            }
+            for post_tag in obj.post_tags.all()
+        ]
