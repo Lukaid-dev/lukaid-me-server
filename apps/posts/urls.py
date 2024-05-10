289ce1f3248372fed835b5eba3a2ed6e922457f5
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-05-10 15:26:25 +0900

    add all posts

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.posts.views import PostId, Posts, PostsPinned, PostsByTag, Tags
+from apps.posts.views import PostId, Posts, PostsAll, PostsPinned, PostsByTag, Tags
 
 urlpatterns = [
     path("", Posts.as_view(), name="posts"),
+    path("all/", PostsAll.as_view(), name="posts_all"),
     path("pinned/", PostsPinned.as_view(), name="posts_pinned"),
     path("<int:pk>/", PostId.as_view(), name="post_id"),
     path("tag/<str:tag_name>/", PostsByTag.as_view(), name="posts_by_tag"),
--- apps/posts/views/__init__.py
+++ apps/posts/views/__init__.py
@@ -1,4 +1,5 @@
 from .posts import *  # noqa
+from .posts_all import *  # noqa
 from .post_id import *  # noqa
 from .posts_pinned import *  # noqa
 from .posts_by_tag import *  # noqa
